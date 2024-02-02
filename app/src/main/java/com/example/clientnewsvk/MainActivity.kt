73795b82f76f4a8ddeae52cdceda08600ba32105
Timur Makhmutov <makhmutov.timur2016@yandex.ru>
2024-02-02 11:47:23 +0300

    news' card basis was made

--- app/src/main/java/com/example/clientnewsvk/MainActivity.kt
+++ app/src/main/java/com/example/clientnewsvk/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.clientnewsvk
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -11,36 +13,20 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.clientnewsvk.ui.theme.ClientNewsVKTheme
+import com.example.clientnewsvk.ui.theme.NewsCard
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ClientNewsVKTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                Box(modifier = Modifier
+                    .fillMaxSize()
+                    .background(MaterialTheme.colorScheme.background)
+                ){
+                    NewsCard()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ClientNewsVKTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/com/example/clientnewsvk/ui/theme/ComposableForHome.kt
+++ app/src/main/java/com/example/clientnewsvk/ui/theme/ComposableForHome.kt
@@ -1,2 +1,149 @@
 package com.example.clientnewsvk.ui.theme
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.MoreVert
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.clientnewsvk.R
+
+@Composable
+fun NewsCard() {
+    ClientNewsVKTheme {
+        Card(
+            modifier = Modifier
+                .background(MaterialTheme.colorScheme.primary)
+                .padding(8.dp)
+                .fillMaxWidth()
+                .background(MaterialTheme.colorScheme.primary),
+            elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
+            shape = RoundedCornerShape(4.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = MaterialTheme.colorScheme.primary,
+                contentColor = MaterialTheme.colorScheme.onPrimary
+            )
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(7.dp)
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.post_comunity_thumbnail),
+                    contentDescription = "",
+                    contentScale = ContentScale.Fit,
+                    modifier = Modifier
+                        .clip(CircleShape)
+                        .size(50.dp)
+                )
+                Column(
+                    modifier = Modifier
+                        .padding(7.dp)
+                        .weight(1f)
+                ) {
+                    Text(text = "уволено", color = MaterialTheme.colorScheme.onPrimary)
+                    Text(text = "14:00", color = MaterialTheme.colorScheme.onSecondary)
+                }
+                Icon(
+                    imageVector = Icons.Rounded.MoreVert,
+                    contentDescription = "",
+                    tint = MaterialTheme.colorScheme.onSecondary
+                )
+            }
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(7.dp)
+            ) {
+                Text(
+                    text = "кабаныч, когда узнал, " +
+                            "что если сотрудникам не платить они начинают умирать от голода"
+                )
+            }
+            Image(
+                painter = painterResource(id = R.drawable.post_content_image),
+                contentDescription = "",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(7.dp),
+                contentScale = ContentScale.FillWidth
+            )
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier.padding(7.dp)
+                ) {
+                    Text(text = "200", modifier = Modifier.padding(start = 7.dp))
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_views_count),
+                        contentDescription = "",
+                        modifier = Modifier.padding(start = 7.dp)
+                    )
+                }
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier.padding(7.dp)
+                ) {
+                    Text(text = "200", modifier = Modifier.padding(start = 7.dp))
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_share),
+                        contentDescription = "",
+                        modifier = Modifier.padding(start = 7.dp)
+                    )
+                    Text(text = "200", modifier = Modifier.padding(start = 7.dp))
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_comment),
+                        contentDescription = "",
+                        modifier = Modifier.padding(start = 7.dp)
+                    )
+                    Text(text = "200", modifier = Modifier.padding(start = 7.dp))
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_like),
+                        contentDescription = "",
+                        modifier = Modifier.padding(start = 7.dp)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun Dark(){
+    ClientNewsVKTheme(darkTheme = true) {
+        NewsCard()
+    }
+}
+
+@Preview
+@Composable
+fun Light(){
+    ClientNewsVKTheme(darkTheme = false) {
+        NewsCard()
+    }
+}
