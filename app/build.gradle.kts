3274150f5a407ed2c4d1766268c0ea0ed54ad263
Timur Makhmutov <makhmutov.timur2016@yandex.ru>
2024-02-18 19:09:43 +0300

    VK sdk was added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,12 @@ android {
 }
 
 dependencies {
+
+    implementation ("com.vk:android-sdk-core:4.1.0")
+    implementation ("com.vk:android-sdk-api:4.1.0")
+
+
+    implementation("io.coil-kt:coil-compose:2.5.0")
     implementation("androidx.compose.runtime:runtime-livedata:1.6.1")
     implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
--- app/src/main/java/com/example/clientnewsvk/MainActivity.kt
+++ app/src/main/java/com/example/clientnewsvk/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.clientnewsvk
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,10 +13,16 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.ui.Modifier
 import com.example.clientnewsvk.ui.MainScreen
 import com.example.clientnewsvk.ui.theme.ClientNewsVKTheme
+import com.vk.api.sdk.VK
+import com.vk.api.sdk.auth.VKAuthenticationResult
+import com.vk.api.sdk.auth.VKScope
 
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
         setContent {
             ClientNewsVKTheme {
                 Box(
@@ -21,6 +30,16 @@ class MainActivity : ComponentActivity() {
                         .fillMaxSize()
                         .background(MaterialTheme.colorScheme.background)
                 ) {
+                    val launcher = rememberLauncherForActivityResult(
+                        contract = VK.getVKAuthActivityResultContract(),
+                        onResult = {
+                            when (it) {
+                                is VKAuthenticationResult.Failed -> TODO()
+                                is VKAuthenticationResult.Success -> TODO()
+                            }
+                        }
+                    )
+                    launcher.launch(listOf(VKScope.WALL))
                     MainScreen()
                 }
             }
