75e429b0fd44d942726a5d85f22ea82f5098b946
Timur Makhmutov <makhmutov.timur2016@yandex.ru>
2024-03-02 22:54:21 +0300

    was fixed coroutines flows into repository

--- app/src/main/java/com/example/clientnewsvk/Extentions.kt
+++ app/src/main/java/com/example/clientnewsvk/Extentions.kt
@@ -3,4 +3,5 @@ package com.example.clientnewsvk
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.merge
 
-fun <T> Flow<T>.mergeWith(another: Flow<T>) = merge(this, another)
+fun <T> Flow<T>.mergeWith(another: Flow<T>) =
+    merge(this, another)
